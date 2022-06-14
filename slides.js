a0c367bcc03baf19fa35d551efa86b068cbabc63
yanalshmilan <yshmilan@gmail.com>
2022-06-14 11:53:53 +0300

    done

--- slides.js
+++ slides.js
@@ -1,26 +1,22 @@
 export default [
-    {
-        id: '1',
-        title: 'Quick & Easy Payments',
-        description: 'Grow your business by accepting card payments with the new card reader.',
-        image: require('./assets/images/creditcard.png'),
-    },
-    {
-        id: '2',
-        title: 'Smart Point of Sale',
-        description: 'Complete point of sale software tailored to your business needs.',
-        image: require('./assets/images/computer.png'),
-    },
-    {
-        id: '3',
-        title: 'Instant Notifications',
-        description: 'Instant notifications let your quickly see new purchases and messages.',
-        image: require('./assets/images/notifications.png'),
-    },
-    {
-        id: '4',
-        title: 'Customize Everything',
-        description: 'Adjust your system to speed up your checkout.',
-        image: require('./assets/images/customize.png'),
-    },
+  {
+    id: '1',
+    title: 'AsyncStorage',
+    description:
+      'AsyncStorage can prove very helpful when we want to save data that the application would need to use, even when the user has closed it or has even closed the device. ',
+    image: require('./assets/images/asyncstorage.png'),
+  },
+  {
+    id: '2',
+    title: 'Simple Storage',
+    description:
+      'The basic actions to perform in AsyncStorage are: set an item, along with its value, retrieve an item’s value, remove an item',
+    image: require('./assets/images/simple.png'),
+  },
+  {
+    id: '3',
+    title: "Let's start with a simple example!",
+    description: "Let's hide this onboarding screen using AsyncStorage.",
+    image: require('./assets/images/start.png'),
+  },
 ];
